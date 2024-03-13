762a60e390a6f6f70f5a8e1415d710163430ee79
ridhoakbar07 <ridhoakbar.id@gmail.com>
2024-03-13 20:30:57 +0800

    add Kelas dan Sekolah Filament Resouce Panel

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Kelas extends Model
 {
     use HasFactory;
+
+    public function sekolah(): BelongsTo
+    {
+        return $this->belongsTo(Sekolah::class);
+    }
 }
